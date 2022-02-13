70780d5c3590691a4a1b4ef276b95ccf5ec6caeb
mweyreuther <mweyreuther@me.com>
2022-02-13 09:46:58 +0100

    1 file fixed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div id="app" class="relative h-screen grid place-items-center bg-gradient-to-tr from-blue-500 to-blue-700">
-  <header class="fixed top-0 left-0 right-0 flex justify-center bg-white/10">      
-    <Button @click="toggleRun" :keepRunning="keepRunning"/>
-  </header>
+  <div
+    id="app"
+    class="
+      relative
+      h-screen
+      grid
+      place-items-center
+      bg-gradient-to-tr
+      from-blue-500
+      to-blue-700
+    "
+  >
+    <header class="fixed top-0 left-0 right-0 flex justify-center bg-white/10">
+      <Button @click="toggleRun" :keepRunning="keepRunning" />
+    </header>
     <div class="mx-8 my-20 max-w-prose">
-      <Fact :keepRunning="keepRunning"/>
+      <Fact :keepRunning="keepRunning" />
     </div>
   </div>
 </template>
@@ -19,18 +30,15 @@ export default {
     Button,
     Fact,
   },
-  data(){
-    return{
-      keepRunning: true
-    }
+  data() {
+    return {
+      keepRunning: true,
+    };
   },
   methods: {
-    toggleRun(){
-      this.keepRunning = !this.keepRunning
-    }
-  }
+    toggleRun() {
+      this.keepRunning = !this.keepRunning;
+    },
+  },
 };
 </script>
-
-<style>
-
